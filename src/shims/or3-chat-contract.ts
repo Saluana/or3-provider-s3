6c2d488aa74f9aadda8c7f30229d57ffca777fa1
Brendon <bgb277@gmail.com>
2026-08-08 00:13:55 -0700

    feat: enhance publishing workflow and add type-checking for release

---
+++ src/shims/or3-chat-contract.ts
@@ -0,0 +1,12 @@
+/** Standalone host contract fixture used by the provider release typecheck. */
+export type StorageGatewayAdapter = any;
+export type PresignUploadRequest = any;
+export type PresignUploadResponse = any;
+export type PresignDownloadRequest = any;
+export type PresignDownloadResponse = any;
+export type DeleteObjectRequest = any;
+export type CanonicalStorageQueryKind = any;
+export type SyncGatewayAdapter = any;
+
+export const getActiveSyncGatewayAdapter = (..._args: any[]): any => undefined;
+export const registerStorageGatewayAdapter = (..._args: any[]): any => undefined;
